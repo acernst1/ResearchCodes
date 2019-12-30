f5bc3d01e686b18144498289891f4c7403d2d1c9
acernst1 <acernst1@gmail.com>
2019-12-30 16:40:42 -0500

    Added histograms to accomodate different beam energy binning

--- DSelector_thrown_kpkpxim.C
+++ DSelector_thrown_kpkpxim.C
@@ -28,6 +28,13 @@ void DSelector_thrown_kpkpxim::Init(TTree *locTree)
 	/******************************** EXAMPLE USER INITIALIZATION: STAND-ALONE HISTOGRAMS *******************************/
 	dHist_Egamma_t = new TH2F("Egamma_t","; E_{#gamma} (GeV); -t (GeV/c)^{2}",180, 3.0, 12.0,1000, 0.0, 5.0);
 	dHist_Egamma_t_acc = new TH2F("Egamma_t_acc","; E_{#gamma} (GeV); -t (GeV/c)^{2}",180, 3.0, 12.0,1000, 0.0, 5.0);
+	dHist_Egamma_t_063 = new TH2F("Egamma_t","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112, 6.3, 11.9,1000, 0.0, 5.0);
+	dHist_Egamma_t_063_acc = new TH2F("Egamma_t_acc","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112, 6.3, 11.9,1000, 0.0, 5.0);
+	dHist_Egamma_t_064 = new TH2F("Egamma_t","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112,6.4, 12.0,1000, 0.0, 5.0);
+	dHist_Egamma_t_064_acc = new TH2F("Egamma_t_acc","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112,6.4, 12.0,1000, 0.0, 5.0);
+	dHist_Egamma_t_065 = new TH2F("Egamma_t","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112,6.5,12.1,1000, 0.0, 5.0);
+	dHist_Egamma_t_065_acc = new TH2F("Egamma_t_acc","; E_{#gamma} (GeV); -t (GeV/c)^{2}",112,6.5,12.1,1000, 0.0, 5.0);
+
 	dHist_KPlus1vsKPlus2_p = new TH2F("KPlus1vsKPlus2_p",";K^{+}_{1} p (GeV);K^{+}_{2} p (GeV)",200,0.0,10.0,200,0.0,10.0);
 	dHist_KPlus1vsKPlus2_theta = new TH2F("KPlus1vsKPlus2_theta",";K^{+}_{1} #theta;K^{+}_{2} #theta",140,0.0,140.0,140,0.0,140.0);
 	dHist_KPlus1_pTheta = new TH2F("KPlus1_ptheta",";K^{+}_{1} #theta; K^{+}_{1} p (GeV);",140,0.0,140.0, 200,0.0,10.0);
--- DSelector_thrown_kpkpxim.h
+++ DSelector_thrown_kpkpxim.h
@@ -31,6 +31,12 @@ class DSelector_thrown_kpkpxim : public DSelector
 		//Histograms
 		TH2F* dHist_Egamma_t;
 		TH2F* dHist_Egamma_t_acc;
+		TH2F* dHist_Egamma_t_063;
+		TH2F* dHist_Egamma_t_063_acc;
+		TH2F* dHist_Egamma_t_064;
+		TH2F* dHist_Egamma_t_064_acc;
+		TH2F* dHist_Egamma_t_065;
+		TH2F* dHist_Egamma_t_065_acc;
 		TH2F* dHist_KPlus1vsKPlus2_p;
 		TH2F* dHist_KPlus1vsKPlus2_theta;
 		TH2F* dHist_KPlus1_pTheta;
