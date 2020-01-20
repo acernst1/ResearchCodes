6f3574e8604af57bfba1a3cb22d54fa837cec5e8
acernst1 <acernst1@gmail.com>
2020-01-20 18:47:28 -0500

    Added input files

--- plot_tslopes.C
+++ plot_tslopes.C
@@ -1,4 +1,14 @@
 void plot_tslopes() {
-
+	TFile * filetslope14 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope14.root");
+	//TFile * filetslope15 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope15.root");
+	//TFile * filetslope16 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope16.root");
+	//TFile * filetslope17 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope17.root");
+	//TFile * filetslope18 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope18.root");
+	TFile * filetslope19 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope19.root");
+	TFile * filetslope20 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope20.root");
+	//TFile * filetslope25 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope25.root");
+	//TFile * filetslope30 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope30.root");
+	//TFile * filetslope35 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope35.root");
+	//TFile * filetslope40 = TFile::Open("/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/kpkpxim__B4_M23_genr8_2018-08_ANAver02_tslope40.root");
 
 }
