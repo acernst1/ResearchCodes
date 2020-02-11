8d431139a90e137ffc6c81d95e0d21a03dfcf9f2
acernst1 <acernst1@gmail.com>
2020-02-11 00:39:09 -0500

    fixed colors array

--- plot_tslope_mass_width.C
+++ plot_tslope_mass_width.C
@@ -10,13 +10,12 @@ char MClegendentry[100];
 
 void plot_tslope_mass_width(){
 int trials[][3] = {{0,18,6},{0,18,8},{0,18,10},{0,20,2},{0,20,4},{0,20,6},{0,20,8},{0,20,10},{0,22,2},{0,22,4},{0,22,6},{0,22,8},{0,22,10},{0,24,2},{0,24,4},{0,24,6},{0,24,8},{0,24,10},{0,26,2},{0,26,4},{0,26,6},{0,26,8},{0,26,10},{0,28,2},{0,28,4},{0,28,6},{0,28,8},{0,28,10},{7,18,2},{7,18,4},{7,18,6},{7,18,8},{7,18,10},{7,20,2},{7,20,4},{7,20,6},{7,20,8},{7,20,10},{7,22,2},{7,22,4},{7,22,6},{7,22,8},{7,22,10},{7,24,2},{7,24,4},{7,24,6},{7,24,8},{7,24,10},{7,26,2},{7,26,4},{7,26,6},{7,26,8},{7,26,10},{7,28,2},{7,28,4},{7,28,6},{7,28,8},{7,28,10},{14,18,2},{14,18,4},{14,18,6},{14,18,8},{14,18,10},{14,20,2},{14,20,4},{14,20,6},{14,20,8},{14,20,10},{14,22,2},{14,22,4},{14,22,6},{14,22,8},{14,22,10},{21,18,2},{35,28,8},{35,28,10}};
-TString colors[] = {kRed,kRed+2,kPink, kPink+2, kMagenta, kMagenta+2, kViolet, kViolet+2, kBlue+2, kCyan, kCyan+2, kTeal, kTeal+2, kGreen, kGreen+2, kSpring, kSpring+2, kYellow, kYellow+2,kOrange, kOrange+2, kGray, kGray+2,{0,28,2},{0,28,4},{0,28,6},{0,28,8},{0,28,10},{7,18,2},{7,18,4},{7,18,6},{7,18,8},{7,18,10},{7,20,2},{7,20,4},{7,20,6},{7,20,8},{7,20,10},{7,22,2},{7,22,4},{7,22,6},{7,22,8},{7,22,10},{7,24,2},{7,24,4},{7,24,6},{7,24,8},{7,24,10},{7,26,2},{7,26,4},{7,26,6},{7,26,8},{7,26,10},{7,28,2},{7,28,4},{7,28,6},{7,28,8},{7,28,10},{14,18,2},{14,18,4},{14,18,6},{14,18,8},{14,18,10},{14,20,2},{14,20,4},{14,20,6},{14,20,8},{14,20,10},{14,22,2},{14,22,4},{14,22,6},{14,22,8},{14,22,10},{21,18,2},{35,28,8},{35,28,10}};
-
-TString colors[] =  {kRed-10, kRed-9, kRed-8, kRed-7, kRed-6, kRed-5,  kRed-4, kRed-3, kRed-2, kRed-1, kRed, kRed+1, kRed+2, kRed+3, kRed+4, kPink-9, kPink-8, kPink-7, kPink-6, kPink-5, kPink-4, kPink-3, kPink-2, kPink-1, kPink, kPink+1, kPink+2, kPink+3,kAzure-9, kAzure-8, kAzure-7, kAzure-6, kAzure-5, kAzure-4, kAzure-3, kAzure-2, kAzure-1, kAzure, kAzure+1, kAzure+2, kAzure+3, kAzure+4, kAzure+5, kAzure+6, kAzure+7, kAzure+8, kAzure+9, kAzure+10, kCyan+4, kCyan+3, kCyan+2, kCyan+1, kCyan, kCyan-1, kCyan-2, kCyan-3, kCyan-4, kCyan-5, kGreen+4, kGreen+3, kGreen+2, kGreen+1, kGreen,  kGreen-1, kGreen-2, kGreen-3, kGreen-4, kGreen-5, kGreen-6, kGreen-7, kGreen-8, kGreen-9, kGreen-10, kBlue+2, kGray, kGray+2};
+int colors[] =  {kRed-10, kRed-9, kRed-8, kRed-7, kRed-6, kRed-5,  kRed-4, kRed-3, kRed-2, kRed-1, kRed, kRed+1, kRed+2, kRed+3, kRed+4, kPink-9, kPink-8, kPink-7, kPink-6, kPink-5, kPink-4, kPink-3, kPink-2, kPink-1, kPink, kPink+1, kPink+2, kPink+3,kAzure-9, kAzure-8, kAzure-7, kAzure-6, kAzure-5, kAzure-4, kAzure-3, kAzure-2, kAzure-1, kAzure, kAzure+1, kAzure+2, kAzure+3, kAzure+4, kAzure+5, kAzure+6, kAzure+7, kAzure+8, kAzure+9, kAzure+10, kCyan+4, kCyan+3, kCyan+2, kCyan+1, kCyan, kCyan-1, kCyan-2, kCyan-3, kCyan-4, kCyan-5, kGreen+4, kGreen+3, kGreen+2, kGreen+1, kGreen,  kGreen-1, kGreen-2, kGreen-3, kGreen-4, kGreen-5, kGreen-6, kGreen-7, kGreen-8, kGreen-9, kGreen-10, kBlue+2, kGray, kGray+2};
 
 TFile * data201701 = TFile::Open("/cache/halld/home/acernst/data/kpkpxim__B4_M23_allbatches_2017-01_ANAver20_347runs_jan02.root");
 TFile * data201801 = TFile::Open("/cache/halld/home/acernst/data/kpkpxim__B4_M23_allbatches_2018-01_ANAver03_543runs_jan02.root");
 TFile * data201808 = TFile::Open("/cache/halld/home/acernst/data/kpkpxim__B4_M23_allbatches_2018-08_ANAver02_449runs_jan02.root");
+
 
 //2017-01 data
 	TH2F * XiMassKinFit_Egamma_t171 = (TH2F*)data201701->Get("Xi_t");
@@ -41,6 +40,7 @@ TFile * data201808 = TFile::Open("/cache/halld/home/acernst/data/kpkpxim__B4_M23
 	legend_datasets->AddEntry(t171,"2017-01_ANAver20 data","lep");
 	legend_datasets->AddEntry(t181,"2018-01_ANAver03 data","lep");
 	legend_datasets->AddEntry(t188,"2018-08_ANAver02 data","lep");
+
 	auto legend_MCtests = new TLegend(0.50,0.6,0.98,0.98);
 	legend_MCtests->SetNColumns(2);
 //No statistics box on histogram 
@@ -72,6 +72,7 @@ for( int i=0; i< sizeof(trials)/sizeof(trials[0]);i++){
 	//Opening MC files
 	sprintf(filename, "/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/temp/kpkpxim__B4_M23_genr8_kpkpxim_2018-08_ANAver02_tslope%02d_mass%02d_width%02d.root",trials[i][0],trials[i][1],trials[i][2]);	
 	sprintf(thrownfilename, "/cache/halld/home/acernst/MC/2018-08_ANAver02/tslope_tests/temp/thrown_kpkpxim__B4_M23_genr8_kpkpxim_2018-08_ANAver02_tslope%02d_mass%02d_width%02d.root",trials[i][0],trials[i][1],trials[i][2]);	
+	cout << "tslope: " << trials[i][0] << " Y: " << trials[i][1] << " +/- " << trials[i][2] << endl;
 	TFile * MCfile = TFile::Open(filename);
 	TFile * Thrownfile = TFile::Open(thrownfilename);
 	//reconstructed MC tests for 2018-08
