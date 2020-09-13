283474784ddc0304333f832eaee8bbc95f6890a1
Ashley Ernst <45637678+acernst1@users.noreply.github.com>
2020-09-13 17:00:11 -0600

    Delete Driver.C

--- Driver.C
+++
@@ -1,4 +0,0 @@
-{
-//void xsec(TString dataFilePath, TString fluxFilePath, TString mcFilePath, TString thrownFilePath1, TString thrownFilePath2, TString thrownFilePath3)
-gROOT->ProcessLine(".x xsec.C(\"kpkpxim_wf_2017_new.root\",\"flux_30274_31057.root\",\"./requestedMC/kpkpxim__B4_M23_genr8.root\",\"./requestedMC/thrown/tree_thrown_genr8_0302_306.root\",\"./requestedMC/thrown/tree_thrown_genr8_0307_309.root\",\"./requestedMC/thrown/tree_thrown_genr8_0310.root\" )");
-}
